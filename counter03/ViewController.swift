aa938415eafee99bd88354df58055aa02a5ff4c3
D7703_07 <d7703_07@lulzsec.local>
2019-03-21 12:59:26 +0900

    힘들었드아아아...

--- counter03/ViewController.swift
+++ counter03/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var myLable: UILabel!
     var cnt : Int = 0
+    var togel : Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,12 +21,22 @@ class ViewController: UIViewController {
     }
 
     @IBAction func CounterPlus(_ sender: UIButton) {
-        if cnt <= 8 {
+        if togel == 0 {
+            if cnt == 8{
+                togel = 1
+            }
             cnt += 1
             myLable.text = String(cnt)
         }
         else{
-            myLable.text = "0"
+            
+            if cnt == 1 {
+                togel = 0
+            }
+            
+            cnt -= 1
+            myLable.text = String(cnt)
+            
         }
     }
     
