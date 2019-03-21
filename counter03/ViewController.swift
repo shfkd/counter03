99db65e74604d72237db0e998d3d6ea850374131
D7703_07 <d7703_07@lulzsec.local>
2019-03-21 12:18:08 +0900

    1~9까지 숫자 변환

--- counter03/ViewController.swift
+++ counter03/ViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myLable: UILabel!
+    var cnt : Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        myLable.text = "0"
     }
 
-
+    @IBAction func CounterPlus(_ sender: UIButton) {
+        if cnt <= 8 {
+            cnt += 1
+            myLable.text = String(cnt)
+        }
+        else{
+            myLable.text = "0"
+        }
+    }
+    
 }
 
